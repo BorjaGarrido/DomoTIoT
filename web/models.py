a55443093ff06099b87528c92575d3710aa590e1
BorjaGarrido <borjagc96@gmail.com>
2019-05-09 22:58:38 +0200

    Primeras clases añadidas en models.py

--- web/models.py
+++ web/models.py
@@ -15,9 +15,23 @@ class Modulo(models.Model):
 		return self.nombre
 
 class dht(Modulo):
-	grados = models.IntegerField(default=None, null=True)
+	temperatura = models.IntegerField(default=None, null=True)
 	humedad = models.IntegerField(default=None, null=True)
 
 class rfid(Modulo):
 	uid = models.CharField(default=None, null=False, max_length= 50)
+
+class mq2(Modulo):
+	lpg = models.IntegerField(default=None, null=True);
+	co2 = models.IntegerField(default=None, null=True);
+	smoke = models.IntegerField(default=None, null=True);
+
+class ldr(Modulo):
+	voltaje = models.IntegerField(default=None, null=True);
+
+class puerta(Modulo):
+	estado = models.BooleanField(default=False);
+
+class luz(Modulo):
+	luminosidad = models.IntegerField(default=0, null=True);
 
