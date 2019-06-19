af03ced63e2f5ec8f56e481ac6ba7014c0f6a8dd
BorjaGarrido <borjagc96@gmail.com>
2019-06-19 19:48:21 +0200

    Añadidos errores formularios registro y login

--- web/urls.py
+++ web/urls.py
@@ -6,9 +6,11 @@ urlpatterns = [
 	path('web/modulos/', views.modulo_list, name='modulo_list'),
 	path('web/contacto/', views.contacto, name='contacto'),
 	path('web/acerca/', views.acerca, name='acerca'),
-    path('web/login/', views.SignInView.as_view(), name='login'),
+    #path('web/login/', views.SignInView.as_view(), name='login'),
+    path('web/login/', views.SignInView, name='login'),
     #path('web/registro/', views.registroUsuario.as_view(), name='registro'),
     path('web/registro/', views.registroUsuario, name='registro'),
-    path('web/logout/', views.SignOutView.as_view(), name='logout'),
+    #path('web/logout/', views.SignOutView.as_view(), name='logout'),
+    path('web/logout/', views.SignOutView, name='logout'),
     path('web/newSensor/', views.newSensor.as_view(), name='nuevo_Sensor'),
 ]
--- web/views.py
+++ web/views.py
@@ -4,9 +4,12 @@ from .models import dht, rfid, mq2, ldr, led, puerta
 from .forms import registroForm, newSensor
 from .models import UserProfile, Modulo
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from django.conf import settings
 from django.shortcuts import redirect
@@ -32,11 +35,8 @@ def inicio(request):
     form_class = registroForm
     success_url = "/"""
 
-class SignInView(LoginView):
-    template_name = 'web/login.html'
-
-class SignOutView(LogoutView):
-    pass
+"""class SignInView(LoginView):
+    template_name = 'web/login.html'"""
 
 class newSensor(CreateView):
     model = Modulo
@@ -53,3 +53,29 @@ def registroUsuario(request):
     else:
         form = registroForm()
     return render(request, 'web/registro.html', {'form': form})
+
+
+def SignInView(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = request.POST['username']
+            passwd = request.POST['password']
+            access = authenticate(username=user, password=passwd)
+            if access is not None:
+                if access.is_active:
+                    login(request, access)
+                    return redirect('/')
+                else:
+                    return render(request, 'web/inactive.html')
+            else:
+                return render(request, 'web/nouser.html')
+    else:
+        form = AuthenticationForm()
+    context = {'form': form}
+    return render(request,'web/login.html', context)
+
+@login_required(login_url='/web/login/')
+def SignOutView(request):
+    logout(request)
+    return redirect('/')
